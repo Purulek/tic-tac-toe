00e2de037148665f400b418c40e141c43d2e2634
purulek <adam.purul@gmail.com>
2024-08-18 17:35:20 +0200

    sixth commit

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,6 @@
 import turtle 
 import random 
-
+import time
 
 
 class board:
@@ -34,8 +34,8 @@ class circle_player:
         self.circle.circle(40)
 
     def where (self):
-        wich_row = turtle.textinput("row")
-        wich_colum = turtle.textinput("colum")
+        wich_row = turtle.textinput("Circle Player","row")
+        wich_colum = turtle.textinput("Circle Player","colum")
         if wich_row == "mid":
             y = -35
             if wich_colum == "mid":
@@ -45,7 +45,7 @@ class circle_player:
             elif wich_colum == "right":
                 x = 115
         elif wich_row == "down":
-            y = -150
+            y = -155
             if wich_colum == "mid":
                 x = 0
             elif wich_colum == "left":
@@ -53,13 +53,80 @@ class circle_player:
             elif wich_colum == "right":
                 x = 115
         elif wich_row == "up":
-            y = 150
-
+            y = 80
+            if wich_colum == "mid":
+                x = 0
+            elif wich_colum == "left":
+                x = -115
+            elif wich_colum == "right":
+                x = 115
+        return [x,y]
     def __call__(self):
         self.circle.penup()
-        self.circle.setposition(0,-155)
+        position = self.where()
+        self.circle.setposition(position[0],position[1])
         self.circle.pendown()
         self.draw_circle()
+
+class cross_player:
+    def __init__(self) -> None:
+        self.cross = turtle.Turtle()
+    
+    def draw_cross(self):
+        self.cross.setheading(-45)
+        self.cross.forward(130)
+        self.cross.penup()
+        self.cross.setposition(self.cross.xcor()-85, self.cross.ycor())
+        self.cross.pendown()
+        self.cross.setheading(45)
+        self.cross.forward(130)
+        time.sleep(3)
+
+
+    def where (self):
+        wich_row = turtle.textinput("Cross Player","row")
+        wich_colum = turtle.textinput("Cross Player","colum")
+        if wich_row == "mid":
+            y = 40
+            if wich_colum == "mid":
+                x = -45
+            elif wich_colum == "left":
+                x = -160
+            elif wich_colum == "right":
+                x = 80
+        elif wich_row == "down":
+            y = -80
+            if wich_colum == "mid":
+                x = -45
+            elif wich_colum == "left":
+                x = - 160
+            elif wich_colum == "right":
+                x = 80
+        elif wich_row == "up":
+            y = 155
+            if wich_colum == "mid":
+                x = -45
+            elif wich_colum == "left":
+                x = -160
+            elif wich_colum == "right":
+                x = 80
+        return [x,y]
+    def __call__(self):
+        self.cross.penup()
+        position = self.where()
+        self.cross.setposition(position[0],position[1])
+        self.cross.pendown()
+        self.draw_cross()
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -67,5 +134,7 @@ class circle_player:
 
 test = board()
 test()
+"""cra = cross_player()
+cra()"""
 dra = circle_player()
 dra()
